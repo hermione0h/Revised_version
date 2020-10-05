df8afd0aa77c24b0160d1e83e758410973d2edf3
Grace Li <hermioneliyu@gmail.com>
2020-10-05 13:14:50 -0400

    Update T60_classification.py

--- T60_classification.py
+++ T60_classification.py
@@ -21,50 +21,10 @@ from torch.nn.utils import clip_grad_norm_
 import torch.nn.parallel.data_parallel as data_parallel
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
-# In[ ]:
 
+t60s = [0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5]
 
-def plot_confusion_matrix(cm,
-                          classes,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix very prettily.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
-
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-
-    # Specify the tick marks and axis text
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=90)
-    plt.yticks(tick_marks, classes)
-
-    # The data formatting
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-
-    # Print the text of the matrix, adjusting text colour for display
-    for i in range (cm.shape[0]):
-        for j in range (cm.shape[1]):
-            plt.text(j, i, format(cm[i, j], fmt),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
-
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-    plt.tight_layout()
-    plt.show()
-    
-#mydict = {0:[1,0,0],1:[0,1,0],2:[0,0,1]}
-
+T60s = np.asarray(t60s)
 
 class ReverbDataset(data.Dataset):
     def __init__(self, file_path):
@@ -75,16 +35,12 @@ class ReverbDataset(data.Dataset):
         mix_speech = dat["mix_speech"]
         speech1 = dat["target1"]
         speech2 = dat["target2"]
-        speech3 = dat["target3"]
-        '''
-        for key,value_list in mydict.items():
-            new_list = np.array(value_list)
-            if np.array_equal(speech2,new_list):
-                new_target2 = key
-        '''
-        return torch.from_numpy(mix_speech).t().type(torch.FloatTensor), torch.tensor(speech1).type(torch.FloatTensor), torch.from_numpy(speech2).type(torch.FloatTensor),torch.tensor(speech3).type(torch.LongTensor)
+        new_out = np.argmax(speech2)
+       
+        return torch.from_numpy(mix_speech).t().type(torch.FloatTensor), torch.tensor(speech1).type(torch.FloatTensor), torch.tensor(new_out).type(torch.LongTensor)
     def __len__(self):
         return len(self.fileList)
+
 
 
 input_path = '/N/slate/liyuy/PROJECTS/DEREVERB3/timit_8k_13/reverb_train/train_wlen_480_nfft_512_overlap_360/reverb_rir5000_roomNum10_t600.3_loc500_8kHz.npz'
@@ -199,9 +155,9 @@ class class_reverb(nn.Module):
         fc_out2 = self.fc2_batchNorm(fc_out2)
         fc_out2 = self.leakyRelu(fc_out2)
         # Predict OneHot vector (classification)
-        final2 = Func.softmax(self.fc3(fc_out2),dim=1)
-        new_final2 = torch.argmax(final2, dim=1)
-        #print(new_final2.shape)
+        final2 = self.fc3(fc_out2)
+       
+        
         out5 = Func.relu(self.conv5(new_in2))
         #print(out5.shape)
         out5 = self.avgpooling(out5)
@@ -216,7 +172,7 @@ class class_reverb(nn.Module):
         final1 = Func.relu(self.fc5(fc_out4))
         final1 = final1.squeeze_()
         #print(final1)
-        return final1, final2, new_final2
+        return final1, final2
         
         
 
@@ -242,28 +198,47 @@ def get_loss(dl, model):
     correction = 0
     total = 0
     
-    for X1, y1,y2,y3 in dl:
-        X1, y1,y2,y3 = Variable(X1).cuda(), Variable(y1).cuda(),Variable(y2).cuda(), Variable(y3).cuda()
-        out1, out2, out3 = model(X1)
-        #out1, out2, out3 = data_parallel(model, (X1))
+    for X1,y1,y2 in dl:
+        X1,y1,y2 = Variable(X1).cuda(),Variable(y1).cuda(),Variable(y2).cuda()
+        out1,out2 = model(X1)
+        out2_v = torch.argmax(out2,dim=1)
+       
+    
+        # regression mse
         ploss1 = criterion1(out1,y1)
-        ploss2 = criterion2(out2,y3)
-        loss1 = param.beta*ploss1 + (1-param.beta)*ploss2
-        loss += loss1.cpu().item() * param.bs
-        correction += out3.eq(y3.data).sum().item()
+        # classification crossentropy
+        ploss2 = criterion2(out2,y2)
+       
+        
+        
+
+        pLoss = param.beta * ploss1 + (1-param.beta) *ploss2 
+        loss += pLoss * param.bs
+        correction += out2_v.eq(y2.data).sum().item()
             #print(correction)
-        total += y3.size(0)
+        total += y2.size(0)
     avgloss = loss / (len(dl.dataset))
     accuracy = correction / total
     return avgloss,accuracy
 
-PATH = "/N/dc2/scratch/liyuy/PROJECTS/DEREVERB3/model_13/model_"+ "%.1f" % param.beta + ".pt"
+PATH = "/N/slate/liyuy/PROJECTS/DEREVERB3/model_13/model_"+ "%.1f" % param.beta + param.new_train+".pt"
 
 #gradient_clip = 5
 
-model.train(True)
+#model.train(True)
 min_loss = np.inf
 best_accuracy = 0.0
+train_loss = []
+val_loss = []
+
+mse_loss = 0
+correction = 0
+total = 0
+
+count = 1
+
+mae_loss = 0
+
 for epoch in range(1,param.num_epoches):
     loss = 0.0
     correction = 0
@@ -271,90 +246,191 @@ for epoch in range(1,param.num_epoches):
     
     model.train(True)
     with torch.set_grad_enabled(True):
-        for sig,src1,src2,new_src2 in train_loader:
-            #print(new_src2.shape)
+        for sig,src1,src2 in train_loader:
+            
             sig = Variable(sig.cuda())  
+            
             src1 = Variable(src1.cuda())
             src2 = Variable(src2.cuda())
-            new_src2 = Variable(new_src2.cuda())
+        
             
-            est1,est2,est3 = model(sig)
-            #print(est2.shape)
-            #est1,est2,est3 = data_parallel(model, (sig))
-            pLoss1 = criterion1(est1,src1)
-            pLoss2 = criterion2(est2,new_src2)
-            pLoss = param.beta * pLoss1 + (1-param.beta) * pLoss2
-            loss += pLoss.cpu().item() * param.bs
-            #print(est3, new_src2)
-            
-            correction += est3.eq(new_src2.data).sum().item()
+            est1,est2= model(sig)
+            est2_v = torch.argmax(est2,dim=1)
+  
+            correction += est2_v.eq(src2.data).sum().item()
             #print(correction)
-            total += new_src2.size(0)
-            #print(total)
+            total += src2.size(0)
+
+            
+            #MSE for regression part
+            pLoss1 = criterion1(est1,src1)
+            #CrossEntropy for classification part
+            pLoss2 = criterion2(est2,src2)
+           
+           
+            
+            pLoss = param.beta * pLoss1 + (1-param.beta) * pLoss2 
+            loss += pLoss * param.bs
+            
             optim.zero_grad()
             pLoss.backward()
-            clip_grad_norm_(model.parameters(), gradient_clip)
+            #clip_grad_norm_(model.parameters(), gradient_clip)
             optim.step()
             
         avgLoss = loss/len(train_loader.dataset)
         train_accuracy = correction/total 
         #print(correction,total,train_accuracy)
+    with torch.no_grad():
         model.eval()
         devLoss, devAccuracy = get_loss(dev_loader,model) 
-        scheduler.step(devLoss)
+    new_avg = avgLoss.cpu().item()
+    new_dev = devLoss.cpu().item()
+    train_loss.append(new_avg)
+    val_loss.append(new_dev)
+        
+        #scheduler.step(devLoss)
     if devLoss < min_loss:
         min_loss = devLoss
-        
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optim.state_dict(),
+        'loss': min_loss,
+        }, PATH)
     if devAccuracy > best_accuracy:
         best_accuracy = devAccuracy
-        torch.save({
-            'epoch': epoch,
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optim.state_dict(),
-            'scheduler': scheduler.state_dict(),
-            'loss': min_loss,
-            }, PATH)
-    print('Epoch {:2}, Train Loss:{:>.9f}, Train Accuracy:{:>.5f}, Validation Loss:{:>.9f}, Validation Accuracy:{:>.5f}, Best Accuracy:{:>.5f}'.format(epoch,avgLoss,train_accuracy,devLoss,devAccuracy,best_accuracy))
+        
+    print('Epoch {:2}, Train Loss:{:>.9f}, Train Accuracy:{:>.5f}, Validation Loss:{:>.9f},Validation Accuracy:{:>.5f}, best accuracy:{:>.5f}'.format(epoch,avgLoss,train_accuracy,devLoss,devAccuracy,best_accuracy))
     #print(epoch)
+save_file = '/N/slate/liyuy/PROJECTS/DEREVERB3/results/loss/loss_'+"%.1f"%param.beta+param.new_train+'.npz'
+np.savez(save_file,
+        train = train_loss,
+        val = val_loss) 
 
+PATH = "/N/slate/liyuy/PROJECTS/DEREVERB3/model_13/model_"+ "%.1f" % param.beta + param.new_train+".pt"
 checkpoint = torch.load(PATH)
 model.load_state_dict(checkpoint['model_state_dict'])
-save_path = '/N/dc2/scratch/liyuy/PROJECTS/DEREVERB3/result/test2/vectors_'+"%.1f"%param.beta+'.npz'
-#optim.load_state_dict(checkpoint['optimizer_state_dict'])
-#start_epoch = checkpoint['epoch']
-#scheduler.load_state_dict(checkpoint['scheduler'])
-#loss = checkpoint['loss']
-model.eval()
+save_path = '/N/slate/liyuy/PROJECTS/DEREVERB3/results/train_results/vectors_'+"%.1f"%param.beta+param.new_train+'.npz'
+
+
 loss = 0
 correction = 0
 total = 0
 target_list = []
 est_list = []
 count = 1
-for X1, y1,y2,y3 in test_loader:
-    
-    X1, y1,y2,y3 = Variable(X1).cuda(), Variable(y1).cuda(),Variable(y2).cuda(), Variable(y3).cuda()
-    
-    out1, out2, out3 = model(X1)
-    #print(out3.shape)
-    for i in range(0,param.bs): 
-        target_list.append(y3[i].data.item())
-        est_list.append(out3[i].data.item())
-        #out1, out2, out3 = data_parallel(model, (X1))
-    ploss1 = criterion1(out1,y1)
-    ploss2 = criterion2(out2,y3)
-    loss1 = param.beta*ploss1 + (1-param.beta)*ploss2
-    loss += loss1.cpu().item() * param.bs
-    correction += out3.eq(y3.data).sum().item()
-            #print(correction)
-    total += y3.size(0)
-np.savez(save_path,
-        target = target_list,
-        est = est_list)    
-avgloss = loss / (len(dev_loader.dataset))
-accuracy = correction / total
-print('Validation Loss:{:>.9f}, Validation Accuracy:{:>.5f}'.format(avgloss,accuracy))
-conf =  confusion_matrix(target_list, est_list)
-classes = ["0.3","0.4","0.5","0.6","0.7","0.8","0.9","1.0","1.1","1.2","1.3","1.4","1.5"]
+T60_est = []
+T60_tru = []
+mae_loss = 0
+mse_c = 0
+mae_c = 0
+save_print = '/N/slate/liyuy/PROJECTS/DEREVERB3/results/train_results/vectors_'+"%.1f"%param.beta+param.new_train+'.txt'
+file1 = open(save_print,'a')
+with torch.no_grad():
+    model.eval()
+    for X1,y1,y2 in train_loader:
 
-plot_confusion_matrix(conf,classes)
+        X1, y1,y2 = Variable(X1).cuda(),  Variable(y1).cuda(),Variable(y2).cuda()
+        #y2_n = torch.argmax(y2,dim=1)
+        out1, out2 = model(X1)
+
+        out2_v = torch.argmax(out2,dim=1)
+        for i in range(0,param.bs): 
+            target_list.append(y2[i].data.item())
+            est_list.append(out2_v[i].data.item())
+            T60_est.append(out1[i].data.item())
+            T60_tru.append(y1[i].data.item())
+  
+
+        # MSE for regression
+        ploss1 = criterion1(out1,y1)
+       # mae for regression
+        ploss3 = mae(out1,y1)
+        
+
+        # MSE for regression
+        loss += ploss1.cpu().item() * param.bs
+        # MAE for regression
+        mae_loss += ploss3.cpu().item() * param.bs
+        
+        correction += out2_v.eq(y2.data).sum().item()
+                #print(correction)
+        total += y2.size(0)
+
+    np.savez(save_path,
+            target = target_list,
+            est = est_list)    
+    mseloss = loss / (len(train_loader.dataset))
+    maeL1 = mae_loss / (len(train_loader.dataset))
+    
+    accuracy = correction / total
+pcc = pearsonr(T60_tru,T60_est)
+srcc = spearmanr(T60_tru,T60_est)
+
+out = 'Train MSE Loss of regression:' + '%.9f' % mseloss + ', MAE Loss of regression:'+'%.9f' % maeL1 + ', PCC:' + '%.5f' % pcc[0] + ', SRCC:' + '%.5f' % srcc[0] + ', Accuracy:' + '%.5f' % accuracy 
+file1.write(out + '\n')
+print('Train MSE Loss of regression:{:>.9f}, MAE Loss of regression:{:>.9f}, PCC:{:>.5f}, SRCC:{:>.5f}, Accuracy:{:>.5f}'.format(mseloss,maeL1,pcc[0],srcc[0],accuracy))
+
+
+
+save_path = '/N/slate/liyuy/PROJECTS/DEREVERB3/results/valid_results/vectors_'+"%.1f"%param.beta+param.new_train+'.npz'
+loss = 0
+correction = 0
+total = 0
+target_list = []
+est_list = []
+count = 1
+T60_est = []
+T60_tru = []
+mae_loss = 0
+mse_c = 0
+mae_c = 0
+#save_print = '/N/slate/liyuy/PROJECTS/DEREVERB3/results/test_results/vectors_'+"%.1f"%param.beta+param.new_train+'.txt'
+#file1 = open(save_print,'a')
+with torch.no_grad():
+    model.eval()
+    for X1,y1,y2 in dev_loader:
+
+        X1,y1,y2 = Variable(X1).cuda(), Variable(y1).cuda(),Variable(y2).cuda()
+        #y2_n = torch.argmax(y2,dim=1)
+        out1, out2 = model(X1)
+        
+        out2_v = torch.argmax(out2,dim=1)
+        for i in range(0,param.bs): 
+            target_list.append(y2[i].data.item())
+            est_list.append(out2_v[i].data.item())
+            T60_est.append(out1[i].data.item())
+            T60_tru.append(y1[i].data.item())
+          
+
+        # MSE for regression
+        ploss1 = criterion1(out1,y1)
+        # Cross Entropy for classification
+        #ploss2 = criterion2(out2,y2)
+        # MAE for regression
+        ploss3 = mae(out1,y1)
+       
+
+        # MSE for regression
+        loss += ploss1.cpu().item() * param.bs
+        # MAE for regression
+        mae_loss += ploss3.cpu().item() * param.bs
+       
+        correction += out2_v.eq(y2.data).sum().item()
+                #print(correction)
+        total += y2.size(0)
+
+    np.savez(save_path,
+            target = target_list,
+            est = est_list)    
+    mseloss = loss / (len(dev_loader.dataset))
+    maeL1 = mae_loss / (len(dev_loader.dataset))
+
+    accuracy = correction / total
+pcc = pearsonr(T60_tru,T60_est)
+srcc = spearmanr(T60_tru,T60_est)
+
+out = 'Valid MSE Loss of regression:' + '%.9f' % mseloss + ', MAE Loss of regression:'+'%.9f' % maeL1 + ', PCC:' + '%.5f' % pcc[0] + ', SRCC:' + '%.5f' % srcc[0] + ', Accuracy:' + '%.5f' % accuracy 
+file1.write(out + '\n')
+print('Valid MSE Loss of regression:{:>.9f}, MAE Loss of regression:{:>.9f}, PCC:{:>.5f}, SRCC:{:>.5f}, Accuracy:{:>.5f}'.format(mseloss,maeL1,pcc[0],srcc[0],accuracy))
+
