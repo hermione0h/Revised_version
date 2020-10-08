e900b3dd35dbc82621547dedc933ac7bf220d9d7
Grace Li <hermioneliyu@gmail.com>
2020-10-08 13:48:46 -0400

    Update T60_classification.py

--- T60_classification.py
+++ T60_classification.py
@@ -22,6 +22,50 @@ import torch.nn.parallel.data_parallel as data_parallel
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 
+
+def plot_confusion_matrix(cm,
+                          classes,
+                          save_file,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix very prettily.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+    plt.figure()
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+
+    # Specify the tick marks and axis text
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=90)
+    plt.yticks(tick_marks, classes)
+
+    # The data formatting
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+
+    # Print the text of the matrix, adjusting text colour for display
+    for i in range (cm.shape[0]):
+        for j in range (cm.shape[1]):
+            plt.text(j, i, format(cm[i, j], fmt),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+    plt.show()
+    plt.savefig(save_file)
+
+
+
 t60s = [0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5]
 
 T60s = np.asarray(t60s)
